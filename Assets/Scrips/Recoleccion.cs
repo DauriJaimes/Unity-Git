de21f5fcd7bd7a3f984cc71a97baa1e31e07aadd
JennyMoreno123 <jennymorenoval@gmail.com>
2025-04-08 09:52:28 -0500

    agregando sonidos al juego

--- Assets/Scrips/Recoleccion.cs
+++ Assets/Scrips/Recoleccion.cs
@@ -8,7 +8,7 @@ public class Recoleccion : MonoBehaviour
     {
         if (other.CompareTag("Recolector"))  // Detecta si tocó un recogible
         {
-            if (efectoVFX != null)
+            if (efectoVFX != null)  // activa el efecto especial
             {
                 Vector3 posicion = transform.position + new Vector3(0, 5f, 0);
                 Instantiate(efectoVFX, posicion, Quaternion.identity);
---
+++ Assets/Scrips/Recolectables.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class Recolectables : MonoBehaviour
+{
+    public AudioClip sonidoMoneda;
+    public GameObject efectoVFXMoneda;
+    public AudioClip sonidoRayo;
+    public AudioClip sonidoPosion;
+    public AudioClip sonidoPulpo;
+    public AudioClip sonidoAncla;
+    public AudioClip sonidoObstaculos;
+    private AudioSource audioSource;
+    public GameObject efectoVFX;
+
+
+    private int score = 0; // puntuación por recoger monedas
+
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>(); // Busca el AudioSource
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Moneda"))  // colision con recolectables
+        {
+            audioSource.PlayOneShot(sonidoMoneda);
+             if (efectoVFXMoneda != null)              // activa el efecto especial
+            {
+                 Vector3 posicion = transform.position + new Vector3(0, 5f, 0);
+                 Instantiate(efectoVFXMoneda, posicion, Quaternion.identity);
+              }
+            score += 1;
+            Destroy(other.gameObject); // desaparición de objetos
+        }
+        else if (other.CompareTag("Rayo"))
+        {
+            audioSource.PlayOneShot(sonidoRayo);
+            if (efectoVFX != null)
+            {
+                Vector3 posicion = transform.position + new Vector3(0, 5f, 0);
+                Instantiate(efectoVFX, posicion, Quaternion.identity);
+            }
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("Posion"))
+        {
+            audioSource.PlayOneShot(sonidoPosion);
+            if (efectoVFX != null)
+            {
+                Vector3 posicion = transform.position + new Vector3(0, 5f, 0);
+                Instantiate(efectoVFX, posicion, Quaternion.identity);
+            }
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("Pulpo"))
+        {
+            audioSource.PlayOneShot(sonidoPulpo);
+            if (efectoVFX != null)
+            {
+                Vector3 posicion = transform.position + new Vector3(0, 5f, 0);
+                Instantiate(efectoVFX, posicion, Quaternion.identity);
+            }
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("Ancla"))
+        {
+            audioSource.PlayOneShot(sonidoAncla);
+            if (efectoVFX != null)
+            {
+                Vector3 posicion = transform.position + new Vector3(0, 5f, 0);
+                Instantiate(efectoVFX, posicion, Quaternion.identity);
+            }
+            Destroy(other.gameObject);
+        }
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstaculo")) // Reproduce sonido al colisionar con obstaculos
+        {
+
+            audioSource.PlayOneShot(sonidoObstaculos);
+        }
+    }
+}
